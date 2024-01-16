be4468d9f0815ee293451fa3c9409fa465931fd9
Vinay kumar <116486037+Vinaykumarmahato@users.noreply.github.com>
2024-01-16 14:24:54 +0530

    Create Solution27.java

---
+++ Solution27.java
@@ -0,0 +1,54 @@
+/*
+Given two integers m and n, try making a special sequence of numbers seq of length n such that
+
+seqi+1 >= 2*seqi 
+seqi > 0
+seqi <= m
+Your task is to determine total number of such special sequences possible.
+
+Example 1:
+
+Input: 
+m = 10
+n = 4
+Output: 
+4
+Explaination: 
+There should be n elements and 
+value of last element should be at-most m. 
+The sequences are {1, 2, 4, 8}, {1, 2, 4, 9}, 
+{1, 2, 4, 10}, {1, 2, 5, 10}.
+Example 2:
+
+Input: 
+m = 5
+n = 2
+Output: 
+6
+Explaination: 
+The sequences are {1, 2}, 
+{1, 3}, {1, 4}, {1, 5}, {2, 4}, {2, 5}.
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function numberSequence() which takes the number m and n as input parameters and returns the number of possible special sequences.
+
+Expected Time Complexity: O(m*n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ m, n ≤ 100
+
+*/
+
+
+class Solution{
+    static int numberSequence(int m, int n){
+        if(m < n){
+            return 0;
+        }
+        if(n == 0){
+            return 1;
+        }
+        return numberSequence(m-1, n) + numberSequence(m/2, n-1);
+    }
+}
+
