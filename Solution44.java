29445c15f6c19f433a43d0ac50d01ca497a8424f
Vinay kumar <116486037+Vinaykumarmahato@users.noreply.github.com>
2024-01-29 16:46:20 +0530

    Create Solution44.java

---
+++ Solution44.java
@@ -0,0 +1,66 @@
+/*
+Given an array A of n non negative numbers. The task is to find the first equilibrium point in an array. Equilibrium point in an array is an index (or position) such that the sum of all elements before that index is same as sum of elements after it.
+
+Note: Return equilibrium point in 1-based indexing. Return -1 if no such point exists. 
+
+Example 1:
+
+Input: 
+n = 5 
+A[] = {1,3,5,2,2} 
+Output: 
+3 
+Explanation:  
+equilibrium point is at position 3 as sum of elements before it (1+3) = sum of elements after it (2+2). 
+Example 2:
+
+Input:
+n = 1
+A[] = {1}
+Output: 
+1
+Explanation:
+Since there's only element hence its only the equilibrium point.
+Your Task:
+The task is to complete the function equilibriumPoint() which takes the array and n as input parameters and returns the point of equilibrium. 
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= n <= 105
+0 <= A[i] <= 109
+
+*/
+
+
+
+class Solution {
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long a[], int n) {
+
+        //We store the sum of all array elements.
+        long sum = 0;
+        for (int i = 0; i < n; i++) 
+           sum += a[i];
+
+        //sum2 is used to store prefix sum.
+        long sum2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            
+            //Leaving out the value of current element from suffix sum.
+            sum = sum - a[i];
+            
+            //Checking if suffix and prefix sums are same.
+            if (sum2 == sum) {
+                //returning the index or equilibrium point.
+                return (i + 1);
+            }
+            
+            //Adding the value of current element to prefix sum.
+            sum2 = sum2 + a[i];
+        }
+        return -1;
+    }
+}
